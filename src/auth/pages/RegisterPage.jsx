96d5bfcca25683d546955b07f4dbf9bee1eac985
Sebastian Gonzalez <seba.gnh@gmail.com>
2022-11-22 17:53:29 -0300

    se agregó color para condo e inicio de formulario para register

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,9 +1,71 @@
 import React from 'react'
+import { Grid, TextField, Typography } from '@mui/material'
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <Grid container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{
+        minHeight: '100vh',
+        backgroundColor: 'blackGray.main', 
+        padding: 4 
+      }}
+    >
+
+      <Grid item
+        className="box-shadow"
+        xs={ 3 }
+        sx={{ 
+          backgroundColor: '#ccc',
+          padding: 3,
+          borderRadius: 2
+        }}
+      >
+        <Typography
+          variant="h5"
+          sx={{
+            mb: 1 //Margin Bottom
+          }}
+        >Login</Typography>
+        
+        <form>
+          <Grid container>
+            <Grid item xs="12" sx={{mt:2}}>
+              <TextField
+                label="correo"
+                type="email"
+                placeholder="correo@correo.com"
+                fullWidth
+              ></TextField>
+            </Grid>
+
+            <Grid item xs="12" sx={{mt:2}}>
+              <TextField
+                label="Password"
+                type="password"
+                placeholder="Password"
+                fullWidth
+              ></TextField>
+            </Grid>
+          </Grid>
+        </form>
+
+      </Grid>
+    </Grid>
   )
 }
 
 export default RegisterPage
+
+/*
+sx hace referencia a "Style X"
+
+xs={3} tendrá 3 posiciones en pantallas pequeñas
+md
+xl
+
+Typography perteneces a material.ui
+*/
--- src/theme/purpleTheme.js
+++ src/theme/purpleTheme.js
@@ -7,6 +7,9 @@ const purpleTheme = createTheme({
     primary:{
       main: '#262254'
     },
+    blackGray:{
+      main: '#292929'
+    },
     secondary:{
       main: '#543884'
     },
