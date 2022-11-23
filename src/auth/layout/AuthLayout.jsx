5dc0b4dd9201662b9c63b92dc3f38c682672a376
Sebastian Gonzalez <seba.gnh@gmail.com>
2022-11-22 23:18:04 -0300

    se crea componente con layout que recibe children

---
+++ src/auth/layout/AuthLayout.jsx
@@ -0,0 +1,48 @@
+import { Grid, Typography } from '@mui/material'
+
+const AuthLayout = ({ children, title="" }) => {
+  return (
+    <Grid container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{
+        minHeight: '100vh',
+        backgroundColor: 'primary.main', 
+        padding: 4 
+        }}
+      >
+
+      <Grid item
+        className="box-shadow"
+        xs={ 3 }
+        sx={{ 
+          backgroundColor: '#ccc',
+          padding: 3,
+          borderRadius: 2,
+          width: { sm: 450 }
+        }}
+      >
+        <Typography
+          variant="h5"
+          sx={{
+            mb: 1 //Margin Bottom
+          }}
+        >{ title }</Typography>
+
+      { children }
+
+      </Grid>
+    </Grid>
+  )
+}
+
+export default AuthLayout
+
+
+/*
+const AuthLayout = ({children, title=""}) => {
+  si no viene el titulo será un string vacío 
+
+*/
--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,79 +1,51 @@
 import { Link as RouterLink} from 'react-router-dom';
 import {Google} from '@mui/icons-material';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material'
+import AuthLayout from '../layout/AuthLayout';
 
 const RegisterPage = () => {
   return (
-    <Grid container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{
-        minHeight: '100vh',
-        backgroundColor: 'primary.main', 
-        padding: 4 
-      }}
-    >
+    <AuthLayout title="Register">
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{mt:2}}>
+            <TextField
+              label="correo"
+              type="email"
+              placeholder="correo@correo.com"
+              fullWidth
+            ></TextField>
+          </Grid>
 
-      <Grid item
-        className="box-shadow"
-        xs={ 3 }
-        sx={{ 
-          backgroundColor: '#ccc',
-          padding: 3,
-          borderRadius: 2
-        }}
-      >
-        <Typography
-          variant="h5"
-          sx={{
-            mb: 1 //Margin Bottom
-          }}
-        >Register</Typography>
-        
-        <form>
-          <Grid container>
-            <Grid item xs={12} sx={{mt:2}}>
-              <TextField
-                label="correo"
-                type="email"
-                placeholder="correo@correo.com"
-                fullWidth
-              ></TextField>
+          <Grid item xs={12} sx={{mt:2}}>
+            <TextField
+              label="Password"
+              type="password"
+              placeholder="Password"
+              fullWidth
+            ></TextField>
+          </Grid>
+
+          <Grid container spacing={2} sx={{my:2}}> 
+            <Grid item xs={6}>
+              <Button variant='contained' fullWidth>Login</Button>
             </Grid>
-
-            <Grid item xs={12} sx={{mt:2}}>
-              <TextField
-                label="Password"
-                type="password"
-                placeholder="Password"
-                fullWidth
-              ></TextField>
-            </Grid>
-
-            <Grid container spacing={2} sx={{my:2}}> 
-              <Grid item xs={6}>
-                <Button variant='contained' fullWidth>Login</Button>
-              </Grid>
-              <Grid item xs={6}>
-                <Button variant='contained' fullWidth>
-                  <Google />
-                  <Typography sx={{ml:1}}>Google</Typography>
-                </Button>
-              </Grid>
-            </Grid>
-
-            <Grid container direction="row" justifyContent="end">
-            <Link  component={RouterLink} color="inherit" to="/auth/login">
-              Crear una cuenta
-            </Link>
+            <Grid item xs={6}>
+              <Button variant='contained' fullWidth>
+                <Google />
+                <Typography sx={{ml:1}}>Google</Typography>
+              </Button>
             </Grid>
           </Grid>
-        </form>
 
-      </Grid>
-    </Grid>
+          <Grid container direction="row" justifyContent="end">
+          <Link  component={RouterLink} color="inherit" to="/auth/login">
+            Crear una cuenta
+          </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
   )
 }
 
